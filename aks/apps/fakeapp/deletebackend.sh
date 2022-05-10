4899c1ddfd6821b4755f2de21ae0a1bbd234a29c
vanphan24 <89482663+vanphan24@users.noreply.github.com>
2022-05-09 18:15:48 -0700

    Delete deletebackend.sh

--- aks/apps/fakeapp/deletebackend.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete deployment backend
-kubectl delete service backend
-kubectl delete serviceaccount backend
-kubectl delete servicedefaults backend
-kubectl delete serviceresolver backend
-kubectl delete serviceintentions backend
